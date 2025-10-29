74e1088ca189a9e189d644352151305ce37a1796
mathzilla390 <matheuslimadasilva251@gmail.com>
2025-10-29 13:39:08 -0300

    Add files via upload

---
+++ at-1.py
@@ -0,0 +1,6 @@
+al1=float(input("Digite a primeira nota: "))
+al2=float(input("Digite a segunda nota: "))
+al3=float(input("Digite a terceira nota: "))
+al4=float(input("Digite a terceira nota: "))
+
+print(f'a media de nota do aluno e igual a: {(al1+al2+al3+al4)/4}')
